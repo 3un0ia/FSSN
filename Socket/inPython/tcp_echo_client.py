2219691c8531030d92806aeac9bd19e7affc6279
3un0ia <kj122kj@naver.com>
2024-10-05 20:25:57 +0900

    fix : Add Exception Handling

--- Socket/inPython/tcp_echo_client.py
+++ Socket/inPython/tcp_echo_client.py
@@ -3,19 +3,32 @@ import socket
 HOST = "127.0.0.1"
 PORT = 65456
 
-print("> echo-client is activated")
+# TCP Client API Procedure
+# socket - connect - recv/send - close
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket :
-    clientSocket.connect((HOST, PORT))
+def main() :
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
 
-    while True :
-        sendMsg = input("> ")
-        clientSocket.sendall(bytes(sendMsg, 'utf-8'))
+        # Add Exception Handling
+        try :
+            if clientSocket.connect((HOST, PORT)) == -1 :
+                print('> connect() failed and program terminated')
+                clientSocket.close()
+                return
+        except Exception as exceptionObj :
+            print('> connect() failed by exception:', exceptionObj)
+            return
 
-        recvData = clientSocket.recv(1024)
-        print("> received:", recvData.decode('utf-8'))
+        while True:
+            sendMsg = input("> ")
+            clientSocket.sendall(bytes(sendMsg, 'utf-8'))
+            recvData = clientSocket.recv(1024)
+            print("> received:", recvData.decode('utf-8'))
 
-        if sendMsg == "quit" :
-            break
+            if sendMsg == "quit":
+                break
 
-print("> echo-client is de-activated")
+if __name__ == "__main__" :
+    print("> echo-client is activated")
+    main()
+    print("> echo-client is de-activated")
--- Socket/inPython/tcp_echo_server.py
+++ Socket/inPython/tcp_echo_server.py
@@ -6,23 +6,41 @@ PORT = 65456
 # TCP Server API Procedure
 # socket - bind - listen - accept - recv/send - close
 
-print("> echo-server is activated")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket :
-    serverSocket.bind((HOST, PORT))
-    serverSocket.listen()
+def main() :
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+        try :
+        # Add Exception Handling
+            if serverSocket.bind((HOST, PORT)) == -1:
+                print('> bind() failed and program terminated')
+                serverSocket.close()
+                return
+        except Exception as exceptionObj :
+            print('> bind() failed by exception:', exceptionObj)
+            serverSocket.close()
+            return
 
-    clientSocket, clientAddress  = serverSocket.accept()
 
-    with clientSocket :
-        print("> client connected by IP address {0} with Port number {1}".format(clientAddress[0], clientAddress[1]))
+        if serverSocket.listen() == -1:
+            print('> listen() failed and program terminated')
+            serverSocket.close()
+            return
 
-        while True :
-            RecvData = clientSocket.recv(1024)
-            print("> echoed :", RecvData.decode('utf-8'))
-            clientSocket.sendall(RecvData)
+        clientSocket, clientAddress = serverSocket.accept()
 
-            if RecvData.decode('utf-8') == 'quit':
-                break
-print("> echo-server is de-activated")
+        with clientSocket:
+            print('> client connected by IP address {0} with Port number {1}'.format(clientAddress[0], clientAddress[1]))
+
+            while True:
+                RecvData = clientSocket.recv(1024)
+                print('> echoed :', RecvData.decode('utf-8'))
+                clientSocket.sendall(RecvData)
+
+                if RecvData.decode('utf-8') == 'quit':
+                    break
+
+if __name__ == "__main__" :
+    print("> echo-server is activated")
+    main()
+    print("> echo-server is de-activated")
 
